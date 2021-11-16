183914509c1d0ce43503c83486bdca3249010f07
Jonathan Sönnerup <nooxet@gmail.com>
2021-11-16 23:08:14 +0100

    Added pch and player class, wip

---
+++ src/Player.cpp
@@ -0,0 +1,133 @@
+﻿#include "zpch.hpp"
+
+#include "Player.hpp"
+
+
+Player::Player()
+{
+	m_speed = START_SPEED;
+	m_health = START_HEALTH;
+	m_maxHealth = START_HEALTH;
+
+	m_texture.loadFromFile("../assets/gfx/player.png");
+	m_sprite.setTexture(m_texture);
+	// set the origin to the sprite center (allows for easy rotation)
+	m_sprite.setOrigin(25, 25);
+}
+
+
+void Player::spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize)
+{
+	// spawn in the center
+	m_position.x = static_cast<float>(arena.width) / 2.f;
+	m_position.y = static_cast<float>(arena.height) / 2.f;
+
+	// copy the arena layout
+	m_arena.left = arena.left;
+	m_arena.width = arena.width;
+	m_arena.top = arena.top;
+	m_arena.height = arena.height;
+
+	m_tileSize = tileSize;
+
+	m_resolution.x = resolution.x;
+	m_resolution.y = resolution.y;
+}
+
+
+void Player::resetPlayerStats()
+{
+	m_speed = START_SPEED;
+	m_health = START_HEALTH;
+	m_maxHealth = START_HEALTH;
+}
+
+
+bool Player::hit(sf::Time timeHit)
+{
+}
+
+
+sf::Time Player::getLastHit()
+{
+}
+
+
+sf::FloatRect Player::getPosition()
+{
+}
+
+
+sf::Vector2f Player::getCenter()
+{
+}
+
+
+float Player::getRotation()
+{
+}
+
+
+void Player::moveLeft()
+{
+}
+
+
+void Player::stopLeft()
+{
+}
+
+
+void Player::moveRight()
+{
+}
+
+
+void Player::stopRight()
+{
+}
+
+
+void Player::moveUp()
+{
+}
+
+
+void Player::stopUp()
+{
+}
+
+
+void Player::moveDown()
+{
+}
+
+
+void Player::stopDown()
+{
+}
+
+
+void Player::update(float dt, sf::Vector2i mousePosition)
+{
+}
+
+
+void Player::upgradeSpeed()
+{
+}
+
+
+void Player::upgradeHealth()
+{
+}
+
+
+void Player::increaseHealthLevel(int amount)
+{
+}
+
+
+int Player::getHealth()
+{
+}
---
+++ src/Player.hpp
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include "SFML/Graphics.hpp"
+
+class Player
+{
+private:
+	const float START_SPEED{ 200 };
+	const int START_HEALTH{ 100 };
+
+	// player data
+	sf::Vector2f m_position;
+	bool m_upPressed;
+	bool m_downPressed;
+	bool m_leftPressed;
+	bool m_rightPressed;
+	int m_health;
+	int m_maxHealth;
+	sf::Time m_lastHit;		// when the player was last hit
+	float m_speed;		// player speed in px/s
+
+	// graphics
+	sf::Sprite m_sprite;
+	sf::Texture m_texture;
+
+	// game info
+	sf::Vector2f m_resolution;	// current screen res
+	sf::IntRect m_arena;		// size of current arena
+	int m_tileSize;			// tile size of the arena
+
+public:
+	Player();
+
+	void update(float dt, sf::Vector2i mousePosition);
+
+	// set player position, angle etc.
+	void spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize);
+
+	// call at the end of every game
+	void resetPlayerStats();
+
+	// handle getting hit by a zombie
+	bool hit(sf::Time timeHit);
+	sf::Time getLastHit();
+
+	// get player position as a rectangle, useful for collision detection
+	sf::FloatRect getPosition();
+	// get player center in <x, y>
+	sf::Vector2f getCenter();
+	// get player facing angle, in degrees
+	float getRotation();
+
+	sf::Sprite getSprite();
+
+	// move/stop moving the player
+	void moveLeft();
+	void stopLeft();
+
+	void moveRight();
+	void stopRight();
+
+	void moveUp();
+	void stopUp();
+
+	void moveDown();
+	void stopDown();
+
+	// player boosts
+	void upgradeSpeed();
+	void upgradeHealth();
+	void increaseHealthLevel(int amount);	// increase the max health
+
+	int getHealth();
+};
